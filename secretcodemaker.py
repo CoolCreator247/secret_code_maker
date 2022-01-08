e345ad3b66bc279c7ffdfd662ef199350086b044
Faris Allafi <farisqaisallafi@gmail.com>
2022-01-08 23:52:44 +0000

    Im done

---
+++ secretcodemaker.py
@@ -0,0 +1,36 @@
+
+print("Welcome to secret code maker!")
+print("1.Eecode a secret code")
+print("2.Decode the secret code")
+
+while True :
+     Option = input(">")
+     
+     if Option =="2":
+          print("insert your text:")
+          code_decode = input(">")
+          lst = []
+          word = code_decode
+          lst.extend(word)
+          print(lst)
+          newString = ""
+          for i in lst:
+               asciiNum = ord(i)
+               asciiChar = chr(asciiNum - 1)
+               newString += asciiChar
+          print(newString)   
+
+     elif Option == "1":
+          print("insert your text:")
+          code_decode = input(">")
+          lst = []
+          word = code_decode
+          lst.extend(word)
+          print(lst)
+          newString = ""
+          for i in lst:
+               asciiNum = ord(i)
+               asciiChar = chr(asciiNum + 1)
+               newString += asciiChar
+          print(newString)     
+
